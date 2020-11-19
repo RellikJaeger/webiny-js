ba31936c581c3fa32ae601519123affff97113b2
Adrian Smijulj <adrian1358@gmail.com>
2020-11-19 10:18:41 +0100

    chore: update env variables

--- apps/admin/code/next.config.js
+++ apps/admin/code/next.config.js
@@ -9,6 +9,13 @@ module.exports = withImages({
     typescript: {
         ignoreBuildErrors: true
     },
+    env: {
+        REACT_APP_USER_POOL_REGION: "eu-central-1",
+        REACT_APP_GRAPHQL_API_URL: "https://d1tsr59p2dxbxy.cloudfront.net/graphql",
+        REACT_APP_API_URL: "https://d1tsr59p2dxbxy.cloudfront.net",
+        REACT_APP_USER_POOL_ID: "eu-central-1_epSCMTYVj",
+        REACT_APP_USER_POOL_WEB_CLIENT_ID: "69u35ll1qqac667bahfp5p7t32"
+    },
     webpack(config) {
         config.module.rules.push({
             test: /\.svg$/,
